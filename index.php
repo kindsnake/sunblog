a65a370c6da0d9b583c8dd65a5122d8cc1951890
kindsnake <kindsnake@163.com>
2020-04-14 18:31:59 +0800

    新建必要文件

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+</head>
+
+<body>
+    <div id="header">
+        <h1 ><a name="toc-3"></a><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+        <?php bloginfo(’description’); ?>
+    </div>
+</body>
+</html>
