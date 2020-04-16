127e546349440543893924dfd7c07157fb8c64af
kindsnake <kindsnake@163.com>
2020-04-16 18:18:39 +0800

    修改了header和footer样式

--- header.php
+++ header.php
@@ -2,9 +2,28 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title><?php bloginfo('name'); ?>-<?php bloginfo('description'); ?></title>
+<title>
+    <?php if ( is_home() ) {
+        bloginfo('name'); echo " - "; bloginfo('description');
+    } elseif ( is_category() ) {
+        single_cat_title(); echo " - "; bloginfo('name');
+    } elseif (is_single() || is_page() ) {
+        single_post_title();
+    } elseif (is_search() ) {
+        echo "搜索结果"; echo " - "; bloginfo('name');
+    } elseif (is_404() ) {
+        echo '页面未找到!';
+    } else {
+        wp_title('',true);
+    } ?>
+</title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>;" type="text/css" media="screen" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0 - 所有文章" href="<?php echo get_bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0 - 所有评论" href="<?php bloginfo('comments_rss2_url'); ?>" />
+<?php wp_head(); ?>
 </head>
+<?php flush(); ?>
 <body>
 <div id="wrapper">
     <div id="header-box">
