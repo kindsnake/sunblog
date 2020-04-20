b7a7aa4d66d0bc7920b35c912f14e7482a679aba
kindsnake <kindsnake@163.com>
2020-04-20 16:41:21 +0800

    更新了底部菜单的样式

--- footer.php
+++ footer.php
@@ -1,8 +1,5 @@
     <div id="footer-box">
-    	<div class="footer-menu">
-    		<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
-    	</div>
-    	<div class="footer-split">&nbsp;</div>
+    	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
         <div id="footer">
             Copyright © 2011-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>, All Rights Reserved. <br />
             Powered by Wordpress. 
