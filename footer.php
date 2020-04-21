b5e7ac68dee0f0bb4cba60aedcd2a47a62494a5e
kindsnake <kindsnake@163.com>
2020-04-21 13:45:40 +0800

    改进了footer menu的组织方式

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
     <div id="footer-box">
-    	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+        <div class="footer-menu">
+    	   <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+        </div>
         <div id="footer">
             Copyright © 2011-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>, All Rights Reserved. <br />
             Powered by Wordpress. 
